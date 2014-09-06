3b1c69b04c3cd26e83ff3d72d7bb0532f5741270
Thomas Habets <thomas@habets.se>
2014-09-06 22:41:38 +0100

    Fix start script to actually start the daemon.

--- extra/startscript.sh
+++ extra/startscript.sh
@@ -2,10 +2,20 @@
 
 set -e
 
+SCANIMAGE="/usr/local/bin/scanimage-wrap"
+DAEMON=/opt/autoscan/bin/autoscan
+PIDFILE=/var/run/autoscan.pid
+USER=thompa
+GROUP=thompa
+DAEMON_ARGS="-scanimage=$SCANIMAGE -templates=/opt/autoscan/templates -static=/opt/autoscan/static -listen=:8080 -config=/opt/autoscan/etc/autoscan.conf"
+
 case "$1" in
     start)
-	echo starting...
 	echo heartbeat > /sys/class/leds/led0/trigger
+	start-stop-daemon --chdir / --background --make-pidfile --start --pidfile "$PIDFILE" --chuid="$USER:$GROUP" --exec "$DAEMON" -- $DAEMON_ARGS
+	;;
+    stop)
+	start-stop-daemon --stop --pidfile "$PIDFILE" # --exec "$DAEMON"
 	;;
     *)
 	echo "Invalid"
